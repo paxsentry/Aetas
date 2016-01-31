af57b83604f53b20d25b0d00ba6a46188c2595b1
paxsentry <zoltan.stomfai@gmail.com>
2016-01-31 18:24:17 +0000

    Clean start.

--- src/Aetas/Startup.cs
+++ src/Aetas/Startup.cs
@@ -9,26 +9,24 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Aetas
 {
-    public class Startup
-    {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-        }
+   public class Startup
+   {
+      // This method gets called by the runtime. Use this method to add services to the container.
+      // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+      public void ConfigureServices(IServiceCollection services)
+      {
+      }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app)
-        {
-            app.UseIISPlatformHandler();
+      // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+      public void Configure(IApplicationBuilder app)
+      {
+         app.Run(async (context) =>
+         {
+            await context.Response.WriteAsync("Hello World!");
+         });
+      }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
-        }
-
-        // Entry point for the application.
-        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
-    }
+      // Entry point for the application.
+      public static void Main(string[] args) => WebApplication.Run<Startup>(args);
+   }
 }
